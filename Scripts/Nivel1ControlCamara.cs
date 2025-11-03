3c186c3aaddf9ca1ee75a99aed7c7eb57418ceac
RHifumi <149911698+RHifumi@users.noreply.github.com>
2025-11-03 15:01:57 -0300

    Update Nivel1ControlCamara.cs

--- Scripts/Nivel1ControlCamara.cs
+++ Scripts/Nivel1ControlCamara.cs
@@ -1,1 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Nivel1ControlCamara : MonoBehaviour
+{
+    public GameObject Jugador;
+    public Vector3 offset;
+
+    void Start()
+    {
+
+        offset = transform.position - Jugador.transform.position;
+
+    }
+
+    void LateUpdate()
+    {
+
+        transform.position = Jugador.transform.position + offset;
+        
+    }
+}
